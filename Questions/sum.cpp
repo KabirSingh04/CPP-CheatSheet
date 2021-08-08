2496c07e9037c39f90c3d4a1fcd7a7931c331284
kabirsingh04 <kabirsingh0405@gmail.com>
2021-08-08 23:30:54 +0530

    added

--- Questions/sum.cpp
+++ Questions/sum.cpp
@@ -5,7 +5,7 @@ int main(int argc, char const *argv[])
     int a;
     int b;
     int sum = 0;
-    cout << "Enter the two numbers to be added";
+    cout << "Enter the two numbers to be added" << endl;
     cin >> a >> b;
     sum = a + b;
     cout << "The sum of " << a << " and " << b << " is : " << sum;
