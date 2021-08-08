f3ae27bd5b26e97abacd6ba6afd5937dd9cc444a
kabirsingh04 <kabirsingh0405@gmail.com>
2021-08-08 21:39:28 +0530

    first commit

---
+++ printNumber.cpp
@@ -0,0 +1,11 @@
+//C++ Program to Print Number Entered by User
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter an integer : " << endl;
+    cin >> a;
+    cout << "The number entered by you is : " << a;
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int a;
+    int b;
+    int sum = 0;
+    cout << "Enter the two numbers to be added";
+    cin >> a >> b;
+    sum = a + b;
+    cout << "The sum of " << a << " and " << b << " is : " << sum;
+    return 0;
+}
